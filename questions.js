208297c26317cb05d8f83dac0a4219dbd150ae48
Sa3nz <53981772+ASAENZ-TE@users.noreply.github.com>
2019-10-06 23:26:12 -0600

    attempted completion of quiz

---
+++ questions.js
@@ -0,0 +1,9 @@
+var questions = [
+    ["One of the most commonly used data types in javascript would be:", "brackets", "manipulations", "strings","cooleans", "3"],
+    ["In a for loop, a variable must be _______ an array length.", "equal to", "less than", "more than", "longer than","2"],
+    ["___________ is another way to write JavaScript using a different syntax.", "jQuery", "JSON", "JAVA", "algorithms","1"],
+    ["JavaScript is considered the _______ of a page.", "bones/structure", "skin/display", "brain/logic", "shortcut","3"],
+    ["Which is not a kind of function?", " named function ", " orphaned function", " anonymous function ", "self invoking function","2"],
+    ["To make a function run, you have to _______ it.", "  recreate", "  summon", " call", " text","3"],
+    ["You can use JavaScript to do everything except:", " regenerate", " set attributes", " create elements", " style","1"],
+    ];
